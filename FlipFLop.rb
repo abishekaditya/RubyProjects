63aef0d76f0d5edf5289324c9980073ae9d39164
Abishek Aditya <abishekaditya@outlook.com>
2016-06-07 19:35:52 +0530

    added the factorial stuff and cleaned my code

--- FlipFLop.rb
+++ FlipFLop.rb
@@ -3,9 +3,9 @@
 DATA = File.new(ARGV[0],"r")
 
 DATA.each do |line|
-    if line =~ /start/ .. line =~ /end/ 
-          puts "flip flop true: #{line}"
-            else
-                  puts "flip flop false: #{line}"
-                    end
+  if line =~ /start/ .. line =~ /end/ 
+    puts "flip flop true: #{line}"
+  else
+    puts "flip flop false: #{line}"
+  end
 end
--- add_file.rb
+++ add_file.rb
@@ -4,6 +4,5 @@ some_file = File.open(ARGV[0],"r")
 
 some_file.each do |line|
   puts line.split.map(&:to_i).reduce(&:+)
-  #                 .inject(0, :+)
 end
 
