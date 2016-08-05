6e02fde24397c70fb3a2d50fb3f02bf92d3127e2
Abishek Aditya <abishekaditya@outlook.com>
2016-08-05 08:45:06 +0530

    postgres

--- factorial.rb
+++ factorial.rb
@@ -1,10 +1,21 @@
 #!/usr/bin/ruby
 
-def fact(n)
-  if n <= 0
-    1
-  else
-    n * fact(n - 1)
+class Fact
+  def fact(n)
+    if n <= 0
+      1
+    else
+      n * fact(n - 1)
+    end
   end
 end
-puts fact(ARGV[0].to_i)
+
+describe Fact do
+  context "when 5 inputted" do
+    it "is 120" do
+      my_fact = Fact.new
+      expect(my_fact.fact(5)).to eq 120
+    end
+  end
+end
+
---
+++ postgres.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+
+require 'pg'
+require 'io/console'
+
+
+conn = PG.connect(:dbname => 'vitdb', :user => 'postgres', :password => STDIN.noecho(&:gets).chomp)
+
+IO.foreach("/home/abishek/PostgresProjects/task1.sql") do |line|
+  conn.exec(line)
+end
+
+ctr = 1
+
+IO.foreach("/home/abishek/PostgresProjects/task2.sql") do |line|
+  puts "Solution #{ctr}"
+  conn.exec(line) do |result|
+    result.each do |row|
+      puts row
+    end
+  puts '----------------'
+  end
+  ctr += 1
+end
+
